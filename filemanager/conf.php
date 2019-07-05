da1d26a830e789cdfee96813f9b97bbf9597e18e
Micdu70 <Micdu70@users.noreply.github.com>
2019-07-05 22:07:56 +0200

    Update 'conf.php' filemanager plugin file

--- filemanager/conf.php
+++ filemanager/conf.php
@@ -4,6 +4,7 @@ $fm['tempdir'] = '/tmp';		// path were to store temporary data ; must be writabl
 $fm['mkdperm'] = 755; 		// default permission to set to new created directories
 
 // set with fullpath to binary or leave empty
+
 if(empty($pathToExternals['rar']))	// May be path already defined?
 {
 	$pathToExternals['rar'] = '';
@@ -33,14 +34,8 @@ if(empty($pathToExternals['bzip2']))	// May be path already defined?
 
 $fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
 
-
-
-
 $fm['archive']['compress'][0] = range(0, 5);
 $fm['archive']['compress'][1] = array('-0', '-1', '-9');
 $fm['archive']['compress'][2] = $fm['archive']['compress'][3] = $fm['archive']['compress'][4] = array(0);
-
-
-
 
 ?>
